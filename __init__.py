4ec4df3e0c121b5666eecbc018d7bbb4eed7d99f
Zach DeCook <zachdecook@gmail.com>
2018-05-17 18:57:07 -0400

    * Sing: Create voice on the fly
    splice audio into backing track

--- __init__.py
+++ __init__.py
@@ -18,11 +18,15 @@
 
 import random
 from os.path import dirname, join
+from subprocess import call
 
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 from mycroft.skills.audioservice import AudioService
 from mycroft.audio import wait_while_speaking
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+from pydub.playback import play
 
 class SingingSkill(MycroftSkill):
     def __init__(self):
@@ -35,6 +39,7 @@ class SingingSkill(MycroftSkill):
             3: join(dirname(__file__), "popey-lose-yourself.mp3"),
             4: join(dirname(__file__), "popey-lovemetender.mp3"),
             5: join(dirname(__file__), "popey-rocketman.mp3"),
+            6: join(dirname(__file__), "amazinggrace"),
         }
 
     def initialize(self):
@@ -47,12 +52,37 @@ class SingingSkill(MycroftSkill):
     @intent_handler(IntentBuilder('').require('Sing'))
     def handle_sing(self, message):
         path = random.choice(self.play_list)
+        path = self.play_list[6]
         try:
             self.speak_dialog('singing')
+            track = self.create_song(path)
             wait_while_speaking()
-            self.audioservice.play(path)
+            play(track)
+            #self.audioservice.play(path)
         except Exception as e:
             self.log.error("Error: {0}".format(e))
+
+    def create_song(path):
+        # Create and load audio
+        # TODO: use tmp file instead of main.wav
+        tmp_voice_file = "/tmp/main.wav"
+        call(['mimic', path + ".ssml", '-o', tmp_voice_file, '-ssml'])
+        voice = AudioSegment.from_wav(tmp_voice_file)
+        track = AudioSegment.from_wav(path + ".wav")
+        # Read the file of spaces
+        with open(path+".time.txt") as f:
+            timing = f.readlines()
+        timing = [int(x.strip()) for x in timing]
+        # Splice the audio together with the spaces.
+        i = 0
+        split_segments = split_on_silence(voice, 100, -64)
+        if len(split_segments) != len(timing):
+            self.log.error("Error: mismatched audio and spacing.")
+        for chunk in split_segments:
+            track = track.overlay(chunk, position=timing[i])
+            i = i+1
+        # Return the AudioSegment so it can be played after we've waited.
+        return track
 
     def stop(self):
         if self.process and self.process.poll() is None:
